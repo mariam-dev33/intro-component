b20964e31404051fd749be15745d4ee1e04170dd
new-m8 <143294084+new-m8@users.noreply.github.com>
2024-06-24 22:46:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
